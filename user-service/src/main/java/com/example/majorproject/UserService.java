ef56ae7bfeeb548f6553e7b4536a90553c27ce9d
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-02 18:47:26 +0530

    Autowired repository beans and implemented methods to get and save user

--- user-service/src/main/java/com/example/majorproject/UserService.java
+++ user-service/src/main/java/com/example/majorproject/UserService.java
@@ -1,14 +1,32 @@
 package com.example.majorproject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    UserCacheRepository userCacheRepository;
+
     public User getUser(String userId) {
-        return null;
+        User user = userCacheRepository.getUser(userId);
+        if (user == null) {
+            user = userRepository.findById(userId);
+            userCacheRepository.addUser(user);
+        }
+        return user;
     }
 
     public void addUser(User user) {
+        userRepository.save(user);
 
+        //save it in the Redis cache
+        userCacheRepository.addUser(user);
+
+        //Publish kafka event for USER_CREATE
     }
 }
