0c10b3a199c4f2572156f79839c2edbdef54ed1c
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-02 18:46:51 +0530

    Implemented JpaRepository interface and declared method to find user by id

--- user-service/src/main/java/com/example/majorproject/UserRepository.java
+++ user-service/src/main/java/com/example/majorproject/UserRepository.java
@@ -1,4 +1,8 @@
 package com.example.majorproject;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findById(String userId);
 }
