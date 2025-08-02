464ec3cd24593f079e3021981057eafcbffc4c4c
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-02 14:22:41 +0530

    Created User entity, UserController & UserService

---
+++ user-service/src/main/java/com/example/majorproject/UserController.java
@@ -0,0 +1,18 @@
+package com.example.majorproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/user")
+    public User getUser(@RequestParam("id") String id){
+        return userService.getUser(id);
+    }
+}
---
+++ user-service/src/main/java/com/example/majorproject/UserService.java
@@ -0,0 +1,14 @@
+package com.example.majorproject;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public User getUser(String userId) {
+        return null;
+    }
+
+    public void addUser(User user) {
+
+    }
+}
