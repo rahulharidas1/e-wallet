1a6d2c1c5dc85ce4db0e9cb48a1da624afd52b3a
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-02 18:47:55 +0530

    Defined POST method to add user

--- user-service/src/main/java/com/example/majorproject/UserController.java
+++ user-service/src/main/java/com/example/majorproject/UserController.java
@@ -1,9 +1,7 @@
 package com.example.majorproject;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
@@ -14,5 +12,10 @@ public class UserController {
     @GetMapping("/user")
     public User getUser(@RequestParam("id") String id){
         return userService.getUser(id);
+    }
+
+    @PostMapping("/user")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
     }
 }
