1287222eda5e6125ce858eda1504e8e397b0a508
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-05 20:47:52 +0530

    Refactored Config class and included functions to get Kafka template instance

--- user-service/src/main/java/com/example/majorproject/UserConfig.java
+++ user-service/src/main/java/com/example/majorproject/UserConfig.java
@@ -1,5 +1,7 @@
 package com.example.majorproject;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
@@ -8,9 +10,14 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Properties;
 
 @Configuration
-public class RedisConfig {
+public class UserConfig {
 
     // To get redis connection
     @Bean
@@ -37,5 +44,25 @@ public class RedisConfig {
         redisTemplate.setConnectionFactory(getRedisFactory());
 
         return redisTemplate;
+    }
+
+    @Bean
+    Properties getKafkaProps() {
+        Properties properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return properties;
+    }
+
+    @Bean
+    ProducerFactory<String, String> getKafkaProducerFactory() {
+        return new DefaultKafkaProducerFactory(getKafkaProps());
+    }
+
+    @Bean
+    KafkaTemplate<String, String> getKafkaTemplate() {
+        return new KafkaTemplate<>(getKafkaProducerFactory());
     }
 }
