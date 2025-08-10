e08c5b386ae5a1e639567d50dc9dc34e35c0b719
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-10 22:07:51 +0530

    Logic for updating wallets once event is consumed when transaction is initiated

--- wallet-service/src/main/java/com/example/majorproject/WalletService.java
+++ wallet-service/src/main/java/com/example/majorproject/WalletService.java
@@ -13,6 +13,8 @@ public class WalletService {
 
     //TODO: Move these variables to application.properties
     private static final String USER_CREATE_TOPIC = "user_create";
+    private static final String TOPIC_TRANSACTION_INITIATED = "transaction_initiated";
+    private static final String WALLET_UPDATE = "wallet_update";
 
     @Autowired
     WalletRepository walletRepository;
@@ -37,5 +39,30 @@ public class WalletService {
 
         //save wallet to DB
         walletRepository.save(wallet);
+    }
+
+    @KafkaListener(topics = {TOPIC_TRANSACTION_INITIATED}, groupId = "transaction-initiated-group")
+    public void updateWalletsForTransaction(String msg) throws JsonProcessingException {
+        JSONObject jsonObject = objectMapper.readValue(msg, JSONObject.class);
+        String fromUser = (String) jsonObject.get("fromUser");
+        String toUser = (String) jsonObject.get("toUser");
+        int amount = (int) jsonObject.get("amount");
+        String transactionId = (String) jsonObject.get("transactionId");
+
+        JSONObject transactionUpdateRequest = new JSONObject();
+        Wallet fromWallet = walletRepository.findByUserId(fromUser);
+
+        transactionUpdateRequest.put("transactionId", transactionId);
+        if (fromWallet == null || fromWallet.getAmount() - amount < 0) {
+            transactionUpdateRequest.put("status", "FAILED");
+        } else {
+            walletRepository.updateWallet(fromUser, 0 - amount);
+            walletRepository.updateWallet(toUser, amount);
+
+            transactionUpdateRequest.put("status", "SUCCESS");
+        }
+
+        // publish wallet_update event for transaction service to consume and mark transaction status
+        kafkaTemplate.send(WALLET_UPDATE, objectMapper.writeValueAsString(transactionUpdateRequest));
     }
 }
