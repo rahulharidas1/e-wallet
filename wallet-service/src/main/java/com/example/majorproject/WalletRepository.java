32dcf5ecf8e8a9a4788aba64feef517a4346f85b
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-10 22:08:23 +0530

    update wallet query defined in wallet repository

--- wallet-service/src/main/java/com/example/majorproject/WalletRepository.java
+++ wallet-service/src/main/java/com/example/majorproject/WalletRepository.java
@@ -1,8 +1,16 @@
 package com.example.majorproject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface WalletRepository extends JpaRepository<Wallet, Integer> {
 
     Wallet findByUserId(String userId);
+
+    @Modifying
+    @Transactional
+    @Query("update Wallet w set w.amount = w.amount + :amount where w.userId = :userId")
+    void updateWallet(String userId, int amount);
 }
