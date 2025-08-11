62d9d4ed7120acfa8456032fd5c57f757afee834
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-11 14:23:44 +0530

    Added required annotations for modifying transaction db

--- transaction-service/src/main/java/com/example/majorproject/TransactionRepository.java
+++ transaction-service/src/main/java/com/example/majorproject/TransactionRepository.java
@@ -1,10 +1,14 @@
 package com.example.majorproject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
 
+    @Transactional
+    @Modifying
     @Query("update Transaction t set t.transactionStatus = :transactionStatus where t.transactionId = :transactionId")
     void updateTransactionByStatus(TransactionStatus transactionStatus, String transactionId);
 }
