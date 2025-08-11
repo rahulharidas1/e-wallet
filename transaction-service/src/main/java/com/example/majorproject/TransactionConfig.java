9dc019a1861eecca4efe7ad09ee5f20c1d3aaae5
Rahul Haridas <rahulharidas06@gmail.com>
2025-08-11 14:22:26 +0530

    Added consumer related beans in TransactionConfig.java

--- transaction-service/src/main/java/com/example/majorproject/TransactionConfig.java
+++ transaction-service/src/main/java/com/example/majorproject/TransactionConfig.java
@@ -1,13 +1,13 @@
 package com.example.majorproject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.*;
 
 import java.util.Properties;
 
@@ -20,6 +20,11 @@ public class TransactionConfig {
         properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        //consumer related properties
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
         return properties;
     }
@@ -37,5 +42,17 @@ public class TransactionConfig {
     @Bean
     ObjectMapper getObjectMapper() {
         return new ObjectMapper();
+    }
+
+    @Bean
+    ConsumerFactory<String, String> getKafkaConsumerFactory() {
+        return new DefaultKafkaConsumerFactory(getKafkaProps());
+    }
+
+    @Bean
+    ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+        concurrentKafkaListenerContainerFactory.setConsumerFactory(getKafkaConsumerFactory());
+        return concurrentKafkaListenerContainerFactory;
     }
 }
